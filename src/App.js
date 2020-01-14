859255d4368258e760820ae4bcb8e9571066742a
Nandang <nandang@local.pc>
2020-01-14 21:19:47 +0700

    0.4 Add MainHeader

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './index.css';
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
     <Router>
+      <MainNavigation />
+      <main>
       <Switch>
         <Route path="/" exact>
           <Users />
@@ -17,6 +20,7 @@ const App = () => {
         </Route>
         <Redirect to="/" />
       </Switch>
+      </main>
     </Router>
   )
 }
